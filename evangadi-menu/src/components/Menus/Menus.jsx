da41802fb9ddb5ffcfcd8de3b46c6ed684a6ed9e
mesud_17 <mesud3818@gmail.com  git config --global user.email mesud3818@gmail.com  git config --global user.email mesud3818@gmail.com>
2026-04-15 11:39:01 +0300

    simple cahnge on Menus.jsx

--- evangadi-menu/src/components/Menus/Menus.jsx
+++ evangadi-menu/src/components/Menus/Menus.jsx
@@ -6,26 +6,26 @@ import data from "../../constant/data";
 export default class Menus extends Component {
   render() {
     return (
+      <>
       <div className={style["foods-container"]}>
         {data.map((item) => {
           const { title, price, img, desc } = item;
 
           return (
             <>
-            <div className="foods-container">
               <MenuList 
               title={title}
               price={price}
               img={img}
               desc={desc}
               />
-            </div>
             
             </>
          
           );
         })}
       </div>
+      </>
     );
   }
 }
